127e1088ea2bf4ae8d5457fd587c9bcebab8a6ee
Jixieee <149821213+Jixieee@users.noreply.github.com>
2024-06-09 23:26:09 +0700

    Add files via upload

---
+++ Job Sheet 1/Percobaan/Percobaan1.cpp
@@ -0,0 +1,12 @@
+/*
+	Nama  : Muhammad Jimmy Al Afif
+	Nim   : 23343046
+	Prodi : Informatika
+*/
+
+#include <stdio.h>
+int main(){
+	printf("\n Welcome to the world of C");	//prints the message on the screen.
+	return 0;	//returns a value 0 to the operating system.
+}
+
